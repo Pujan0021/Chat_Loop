fd4f9be5f8600b6a171918df28f9ce030ab13739
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-25 20:46:29 +0545

    Connected TO DataBase

--- BackEnd/src/lib/db.js
+++ BackEnd/src/lib/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
+
     try {
         await mongoose.connect(process.env.Mongo_URL);
-        console.log("SuccessFully Connected To MongoDB")
+        console.log("----- Connected To DataBase -------")
     } catch (err) {
-        console.log("Error Connecting To MongoDB");
+        console.log("!!!!!!!!!!!!!!!!!!! Error Connecting To DataBase !!!!!!!!!!!");
         process.exit(1); //1 means failed and 0 means success
     }
 }
