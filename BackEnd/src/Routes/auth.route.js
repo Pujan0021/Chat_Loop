f05e390f1c0fecd0e530dc8db6ce479f64805eb7
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-29 13:09:39 +0545

    Auth Route Refactored

--- BackEnd/src/Routes/auth.route.js
+++ BackEnd/src/Routes/auth.route.js
@@ -5,16 +5,11 @@ const arcjetProtection = require("../middleware/arcjet.middleware.js");
 const { signUp, logIn, logOut, updateProfilePic } = require("../Controller/auth.controller.js");
 const protectRoute = require("../middleware/auth.middleware.js");
 
-// Define routes separately
+
+Router.use(arcjetProtection);
 Router.post("/signup", signUp);
 Router.post("/logout", logOut);
 Router.post("/login", arcjetProtection, logIn);
-Router.get("/test", arcjetProtection, ((req, res) => {
-    res.status(200).json({
-        message: "Test Successful"
-    })
-
-}));
 Router.put("/update-profile", protectRoute, updateProfilePic);
 
 module.exports = Router;
