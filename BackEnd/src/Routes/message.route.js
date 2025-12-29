a0de681ca6bf60bd0e009b88281ead9ca5e06e1e
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-29 21:52:34 +0545

    Get Chat Partners Route

--- BackEnd/src/Routes/message.route.js
+++ BackEnd/src/Routes/message.route.js
@@ -1,9 +1,9 @@
 const protectRoute = require("../middleware/auth.middleware.js")
 const express = require("express");
 const Router = express.Router();
-const { getAllContacts, getMessageByUserId, sendMessage } = require("../Controller/message.controller.js")
+const { getAllContacts, getMessageByUserId, sendMessage, getChatPartners } = require("../Controller/message.controller.js")
 Router.get("/contacts", protectRoute, getAllContacts);
-// Router.get("/chats", getChatPartners);
+Router.get("/chats", protectRoute, getChatPartners);
 Router.get("/:id", protectRoute, getMessageByUserId);
 Router.post("/send/:id", protectRoute, sendMessage);
 
