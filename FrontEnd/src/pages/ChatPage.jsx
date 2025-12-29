a0b01b1767c7af63661e158f80d995299b6eb2a5
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-29 23:31:00 +0545

    Initialized FrontEnd

---
+++ FrontEnd/src/pages/ChatPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatPage = () => {
+  return <div className="text-cyan-50">This is a chatpage</div>;
+};
+
+export default ChatPage;
---
+++ FrontEnd/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div>This is a signup page</div>;
+};
+
+export default SignUp;
