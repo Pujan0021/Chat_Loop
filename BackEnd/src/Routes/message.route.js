280bb3c56f01562ac3e54a34a826dc532c997797
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-29 21:56:54 +0545

    Get Chat Partners Route Refactored

--- BackEnd/src/Routes/message.route.js
+++ BackEnd/src/Routes/message.route.js
@@ -1,8 +1,9 @@
 const protectRoute = require("../middleware/auth.middleware.js")
 const express = require("express");
 const Router = express.Router();
-Router.use(protectRoute);
-const { getAllContacts, getMessageByUserId, sendMessage, getChatPartners } = require("../Controller/message.controller.js")
+const arcjetProtection = require("../middleware/arcjet.middleware.js");
+Router.use(arcjetProtection, protectRoute);
+const { getAllContacts, getMessageByUserId, sendMessage, getChatPartners } = require("../Controller/message.controller.js");
 Router.get("/contacts", getAllContacts);
 Router.get("/chats", getChatPartners);
 Router.get("/:id", getMessageByUserId);
