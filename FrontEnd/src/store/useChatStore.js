114632b121a0a6d131fe42ad66e72a703aa32757
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-02 22:48:42 +0545

    Chat Store

---
+++ FrontEnd/src/store/useChatStore.js
@@ -0,0 +1,45 @@
+import { create } from "zustard";
+import axiosInstance from "../lib/axios.js";
+import toast from "react-hot-toast";
+const useChatStore = create((set, get) => ({
+    allContacts: [],
+    chats: [],
+    messages: [],
+    activeTab: "chats",
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    isSoundEnable: localStorage.getItem("isSoundEnable") === true,
+    toggleSound: () => {
+        localStorage.setItem("isSoumdEnable", !get().isSoundEnable)
+        set({ isSoundEnable: !get().isSoundEnable })
+
+    },
+    setActiveTab: (tab) => set({ activeTab: tab }),
+    setSelectedUser: (selectedUser) => set({ selectedUser }),
+    getAllContacts: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/contact");
+            set({ allContacts: res.data })
+        } catch (error) {
+            toast.error(error.response.data.message)
+
+        } finally {
+            set({ isUsersLoading: false })
+        }
+    },
+    getMyChatPatterns: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/contact");
+            set({ chats: res.data })
+        } catch (error) {
+            toast.error(error.response.data.message)
+
+        } finally {
+            set({ isUsersLoading: false })
+        }
+    }
+}))
+export default useChatStore
