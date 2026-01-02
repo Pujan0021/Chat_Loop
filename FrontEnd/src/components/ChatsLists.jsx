b473dc34d21295daea40a42b7cf2201e0a84c5f2
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-02 22:58:17 +0545

    Components Added

---
+++ FrontEnd/src/components/ChatsLists.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatsLists = () => {
+  return <div></div>;
+};
+
+export default ChatsLists;
--- FrontEnd/src/pages/ChatPage.jsx
+++ FrontEnd/src/pages/ChatPage.jsx
@@ -4,10 +4,15 @@ const ChatPage = () => {
   const { logout } = useAuthStore();
   return (
     <>
-      <div className="text-cyan-50">This is a chatpage.</div>
-      <button className="z-10" onClick={logout}>
-        LogOut
-      </button>
+      <div className="relative w-full max-w-6xl h-(800px)">
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab == "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
