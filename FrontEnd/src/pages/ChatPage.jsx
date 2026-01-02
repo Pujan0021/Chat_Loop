d2b9d4de039ec80c492e09196fa0b054b10397f4
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-02 23:03:51 +0545

    Components Added

--- FrontEnd/src/pages/ChatPage.jsx
+++ FrontEnd/src/pages/ChatPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import useAuthStore from "../store/useAuthStore.js";
+import ProfileHeader from "../components/ProfileHeader.jsx";
+import ActiveTabSwitch from "../components/ActiveTabSwitch.jsx";
+import ChatsLists from "../components/ChatsLists.jsx";
+import ChatContainer from "../components/ChatContainer.jsx";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder.jsx";
 const ChatPage = () => {
   const { logout } = useAuthStore();
   return (
@@ -9,8 +14,12 @@ const ChatPage = () => {
           <ProfileHeader />
           <ActiveTabSwitch />
           <div className="flex-1 overflow-y-auto p-4 space-y-2">
-            {activeTab == "chats" ? <ChatsList /> : <ContactList />}
+            {activeTab == "chats" ? <ChatsLists /> : <ContactList />}
           </div>
+        </div>
+
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
         </div>
       </div>
     </>
