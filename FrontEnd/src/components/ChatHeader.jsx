978117bf453e4b4a5a58de2051548fbf5be368e2
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-04 22:50:02 +0545

    No Conversation Placerholder

--- FrontEnd/src/components/ChatHeader.jsx
+++ FrontEnd/src/components/ChatHeader.jsx
@@ -13,9 +13,12 @@ const ChatHeader = () => {
     return () => window.removeEventListener("keydown", handleEscKey);
   }, [setSelectedUser]);
   return (
-    <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-(84px) px-6 flex-1">
+    <div
+      className="flex justify-between items-center bg-slate-800/50 border-b
+   border-slate-700/50 max-h-[84px] px-6 flex-1"
+    >
       <div className="flex items-center space-x-3">
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : "offline"}`}>
           <div className="w-12 rounded-full">
             <img
               src={selectedUser.profilePic || "/avatar.png"}
@@ -23,13 +26,17 @@ const ChatHeader = () => {
             />
           </div>
         </div>
+
         <div>
           <h3 className="text-slate-200 font-medium">
             {selectedUser.fullName}
           </h3>
-          <p className="text-slate-400 text-sm">Online</p>
+          <p className="text-slate-400 text-sm">
+            {isOnline ? "Online" : "Offline"}
+          </p>
         </div>
       </div>
+
       <button onClick={() => setSelectedUser(null)}>
         <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
       </button>
